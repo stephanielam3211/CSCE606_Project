a50d6c785acdf97ab2888128da37e29f5227ffdf
Yuvanesh Rajamani <jjd149@txstate.edu>
2025-02-10 18:01:07 -0600

    more submission form test

--- spec/tests/applicant_spec.rb
+++ spec/tests/applicant_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe Applicant, type: :model do
     applicant = Applicant.new(email: "john@example.com", degree: "PhD")
     expect(applicant).not_to be_valid
   end
+  
+  it "is invalid without an email" do
+    applicant = Applicant.new(name: "John Doe", degree: "PhD", positions:"TA", number:"512-555-5555",
+    uin:"123456789", hours:"13", citizenship:"USA", cert:"1")
+    expect(applicant).not_to be_valid
+  end
+
+  it "is invalid without a degree" do
+    applicant = Applicant.new(name: "John Doe", email: "john@example.com", positions:"TA", number:"512-555-5555",
+    uin:"123456789", hours:"13", citizenship:"USA", cert:"1")
+    expect(applicant).not_to be_valid
+  end
+
+  it "is invalid without an uin" do
+    applicant = Applicant.new(name: "John Doe", email: "john@example.com", degree: "PhD", positions:"TA", number:"512-555-5555",
+    hours:"13", citizenship:"USA", cert:"1")
+    expect(applicant).not_to be_valid
+  end
+
+  it "is invalid without a position" do
+    applicant = Applicant.new(name: "John Doe", email: "john@example.com", degree: "PhD", number:"512-555-5555",
+     uin:"123456789", hours:"13", citizenship:"USA", cert:"1")
+    expect(applicant).not_to be_valid
+  end
+
+  it "is invalid without a hours" do
+    applicant = Applicant.new(name: "John Doe", email: "john@example.com", degree: "PhD", positions:"TA", number:"512-555-5555",
+     uin:"123456789", citizenship:"USA", cert:"1")
+    expect(applicant).not_to be_valid
+  end
 end
