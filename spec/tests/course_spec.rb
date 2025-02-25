86d457b45f4130de142510c596ee0e65ce672850
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-02-24 19:05:10 -0600

    added some rspec tests

---
+++ spec/tests/course_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature "Course Search", type: :feature do
+    let!(:course) { Course.create!(course_name: "CSCE 606", course_number: "606", section: "500", instructor: "Dr.richey", faculty_email:"rrrr@tamu.edu" ) }
+  
+    scenario "User searches for a course" do
+      visit courses_path
+      expect(Course.count).to be > 0 
+  
+      fill_in "Search by Course Details", with: "CSCE 606"
+      click_button "Search"
+      expect(page).to have_css("td", text: "CSCE 606") 
+    end
+
+    scenario "User searches for a course number" do
+      visit courses_path
+      expect(Course.count).to be > 0 
+  
+      fill_in "Search by Course Details", with: "606"
+      click_button "Search"
+      expect(page).to have_css("td", text: "606") 
+    end
+
+    scenario "User searches for a section" do
+      visit courses_path
+      expect(Course.count).to be > 0 
+  
+      fill_in "Search by Course Details", with: "500"
+      click_button "Search"
+      expect(page).to have_css("td", text: "500") 
+    end
+
+    scenario "User searches for a Instructor" do
+      visit courses_path
+      expect(Course.count).to be > 0 
+  
+      fill_in "Search by Course Details", with: "Dr.richey"
+      click_button "Search"
+      expect(page).to have_css("td", text: "Dr.richey") 
+    end
+  end
