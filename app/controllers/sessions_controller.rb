6494ce5be4711fb4bf086f97468f30688d2d4aa7
Aaron Xu <aaron.c.xu92@gmail.com>
2025-03-24 17:23:35 -0500

    fixed login page error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  protect_from_forgery except: :create
-  skip_before_action :verify_authenticity_token, only: [:create]
-  
   def create
-    auth = request.env['omniauth.auth']
+    auth = request.env["omniauth.auth"]
     user = User.from_google(auth)
-    session[:user_id] = user.id
-    session[:user] = user.name
-    session[:role] = 'admin'
-    redirect_to root_path, notice: "Signed in successfully!"
-  rescue => e
-    redirect_to root_path, alert: e.message
+
+    if user
+      session[:user_id] = user.id
+      session[:user] = { name: user.name, email: user.email }
+      redirect_to root_path
+    else
+      flash[:alert] = "You must use a @tamu.edu email address to log in."
+      redirect_to root_path, status: :see_other
+    end
   end
 
   def destroy
     reset_session
-    redirect_to root_path, notice: "Signed out successfully!"
+    redirect_to root_path, status: :see_other
   end
 end
