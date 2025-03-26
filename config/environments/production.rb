b60df07ecbb93db34c4a47f7c434c604dff96eec
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-03-26 10:30:06 -0500

    for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,18 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['SMTP_EMAIL'],
+    password:ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    open_timeout: 30,
+    read_timeout: 30
+  }
+  config.action_mailer.default_url_options = { host: "tamu-ta.herokuapp.com" }
+
 end
