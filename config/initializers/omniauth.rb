029a835189d7f9f83297eadb48ff50f14b02e5e0
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-03-27 11:06:04 -0500

    for deplyment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
              ENV['GOOGLE_CLIENT_ID'],
              ENV['GOOGLE_CLIENT_SECRET'],
              scope: 'email,profile',
-             prompt: 'select_account'#,
+             prompt: 'select_account',
              #COMMENT OUT THE LINE BELOW TO RUN LOCALLY
-             #redirect_uri: 'https://tamu-ta-ee36b085db2d.herokuapp.com/auth/google_oauth2/callback'
+             redirect_uri: 'https://tamu-ta-ee36b085db2d.herokuapp.com/auth/google_oauth2/callback'
   end
   
