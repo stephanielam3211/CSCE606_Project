0919838ff9a6f8040989e1bfc1935b9a4f05993c
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-04-29 16:14:56 -0500

    changing to run after precompiling

--- config/initializers/download_after_restart.rb
+++ config/initializers/download_after_restart.rb
@@ -1,17 +1,17 @@
 require Rails.root.join('lib/s3_downloader')
 
 Rails.application.config.after_initialize do
-    if Rails.env.production? || Rails.env.staging?
+    if (Rails.env.production? || Rails.env.staging?) && ENV['AWS_REGION'].present?
       puts "Downloading CSV files from S3..."
   
-      # Specify the directory to save files locally
       directory_path = Rails.root.join("app/Charizard/util/public/output")
   
-      # Create the downloader and trigger download
       downloader = S3FileDownloader.new(
         bucket_name: ENV['BUCKETEER_BUCKET_NAME'],
         directory_path: directory_path
       )
       downloader.download_files
+    else
+      puts "Skipping S3 download – AWS_REGION not set or wrong environment."
     end
-end
+  end
