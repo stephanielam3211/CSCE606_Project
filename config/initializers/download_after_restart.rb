5f160a62aaa5a796be7689ee6618ad7685d7c082
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-04-29 19:51:12 -0500

    moving class

--- config/initializers/download_after_restart.rb
+++ config/initializers/download_after_restart.rb
@@ -1,4 +1,29 @@
-require Rails.root.join('lib/s3_downloader')
+require 'aws-sdk-s3'
+require 'fileutils'
+
+class S3Downloader
+  def initialize(bucket_name:)
+    @bucket_name = bucket_name
+    @s3_client = Aws::S3::Client.new(region: ENV['BUCKETEER_AWS_REGION'])
+  end
+
+  def download_files
+    s3 = Aws::S3::Resource.new(client: @s3_client)
+    bucket = s3.bucket(@bucket_name)
+
+    bucket.objects.each do |obj|
+      # Build the directory path dynamically using the key
+      directory_path = Rails.root.join('app', 'Charizard', 'util', 'public', 'output')
+      filename = File.join(directory_path, File.basename(obj.key)) # Use the object's key here
+
+      puts "Downloading: #{obj.key} -> #{filename}"  # 🔹 Add log here
+      FileUtils.mkdir_p(File.dirname(filename))  # Ensure the directory exists
+      obj.get(response_target: filename)  # Download the file
+    rescue => e
+      puts "Failed to download #{obj.key}: #{e.message}"  # 🔹 Log errors too
+    end
+  end
+end
 
 Rails.application.config.to_prepare do
     if (Rails.env.production? || Rails.env.staging?) && ENV['BUCKETEER_AWS_REGION'].present?
