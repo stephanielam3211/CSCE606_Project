6b2ddc02f36dd7351bfc5258dc456067cb0d5be2
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-04-30 16:21:16 -0500

    Pushing form validations

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -12,15 +12,27 @@ class Applicant < ApplicationRecord
   validates :degree, presence: true
   validates :positions, presence: true
   validates :number, presence: true, format: { with: /\A\+?[0-9\-]{10,15}\z/, message: "must be a valid phone number" }
-  validates :uin, presence: true
+  validates :uin, presence: true,format: {
+              with: /\A\d{9}\z/,
+              message: "must be exactly 9 digits"
+            }
   validates :hours, presence: true, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 16.0 }
   validates :citizenship, presence: true
   validates :cert, presence: true
   validates :gpa, presence: true, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 4.0 }
   validate :min_course_choice
-  validates :prev_course, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
+  validates :prev_course, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" },
+            format: { 
+              with: /\A\d{3}(,\d{3})*\z/,
+              message: "Must Only Include Course Numbers\ '123,345,456'"
+            }
+  validate :check_duplicates
   validates :prev_uni, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
-  validates :prev_ta, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
+  validates :prev_ta, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" },
+            format: { 
+              with: /\A\d{3}(,\d{3})*\z/,
+              message: "Must Only Include Course Numbers Numbers\ '123,345,456'"
+            }
   # type casting
   ransacker :uin do |parent|
     Arel.sql("CAST(#{parent.table.name}.uin AS TEXT)")
@@ -40,6 +52,20 @@ class Applicant < ApplicationRecord
 
   def self.ransackable_associations(auth_object = nil)
     []
+  end
+
+  def check_duplicates
+    return if prev_course.blank? || prev_ta.blank?
+    
+    prev_numbers = prev_course.split(",")
+    prev_ta_numbers = prev_ta.split(",")
+
+    if prev_numbers.uniq.length != prev_numbers.length
+      errors.add(:prev_course, "must not contain duplicate course numbers")
+    end
+    if prev_ta_numbers.uniq.length != prev_ta_numbers.length
+      errors.add(:prev_ta, "must not contain duplicate course numbers")
+    end
   end
 
   private
