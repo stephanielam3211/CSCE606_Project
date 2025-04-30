0db5a544db9b4504899491c3df25f727734403d1
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-04-29 19:53:50 -0500

    testing where the bug is

--- config/initializers/download_after_restart.rb
+++ config/initializers/download_after_restart.rb
@@ -25,17 +25,4 @@ class S3Downloader
   end
 end
 
-Rails.application.config.to_prepare do
-    if (Rails.env.production? || Rails.env.staging?) && ENV['BUCKETEER_AWS_REGION'].present?
-      puts "Downloading CSV files from S3..."
-  
-      #directory_path = Rails.root.join("app/Charizard/util/public/output")
-  
-      downloader = S3Downloader.new(
-        bucket_name: ENV['BUCKETEER_BUCKET_NAME']
-      )
-      downloader.download_files
-    else
-      puts "Skipping S3 download – AWS_REGION not set or wrong environment."
-    end
-  end
+
