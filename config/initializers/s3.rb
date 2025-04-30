fc8ecbf4304388d00b57f0119710320ccbe03967
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-04-29 21:32:41 -0500

    updating for local development

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,11 +1,15 @@
-require 'aws-sdk-s3'
+if ENV['RACK_ENV'] == 'production'
+  require 'aws-sdk-s3'
 
-Aws.config.update({
-  region: ENV['BUCKETEER_AWS_REGION'] || 'us-east-1',
-  credentials: Aws::Credentials.new(
-    ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
-    ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
-  )
-})
+  Aws.config.update({
+    region: ENV['BUCKETEER_AWS_REGION'] || 'us-east-1',
+    credentials: Aws::Credentials.new(
+      ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+      ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+    )
+  })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKETEER_BUCKET_NAME'])
+  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKETEER_BUCKET_NAME'])
+else
+  S3_BUCKET = nil
+end
