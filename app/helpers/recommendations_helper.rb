eb39f09d7be9603d0b06b20df9eb91d4ed45eccd
Yuvanesh Rajamani <yuvi@tamu.edu>
2025-05-16 12:16:12 -0500

    updating recommendations

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module RecommendationsHelper
+  def recommendation_class(feedback)
+    case feedback
+    when "I want to work with this student"
+        "yes-recommendation"
+    when "I would recommend this person as a good TA/grader"
+        "ok-recommendation"
+    when "I would not recommend this student"
+        "bad-recommendation"
+    else
+        "no-recommendation"
+    end
+  end
 end
